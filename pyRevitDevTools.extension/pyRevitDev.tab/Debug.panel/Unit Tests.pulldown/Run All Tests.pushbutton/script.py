340398fbca0b489d88df95bae52b5aa73fb45fa6
Vitaclick <vitaliy.zema@gmail.com>
2018-08-30 17:13:32 +0300

    CleanUp

--- pyRevitDevTools.extension/pyRevitDev.tab/Debug.panel/Unit Tests.pulldown/Run All Tests.pushbutton/script.py
+++
@@ -1,8 +0,0 @@
-"""Unit Tests for pyRevit."""
-
-import pyrevit.unittests as prtests
-
-prtests.perform_engine_tests()
-prtests.perform_coreutils_tests()
-prtests.perform_script_tests()
-prtests.perform_revit_tests()
